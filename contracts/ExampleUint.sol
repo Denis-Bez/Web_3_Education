0470799499e3866148b13680812298ee86e41d26
Denis-Bez <v417459@yandex.ru>
2023-06-02 19:36:09 +0500

    Education Lesson 20 (Chapter 2)

--- contracts/ExampleUint.sol
+++ contracts/ExampleUint.sol
@@ -4,11 +4,26 @@ pragma solidity 0.8.18;
 
 contract ExampleUint {
 
-    uint256 public myUint;
+    uint256 public myUint = 2**4; // 0 - (2*256) - 1
 
-    uint8 public myUint8;
+    uint8 public myUint8 = 250;
+
+    int public myInt = -10; // -2*128 to 2*128 - 1
+
+    function decreementUint() public {
+        myUint--;
+    }
 
     function setMyUint(uint _myUint) public {
         myUint = _myUint;
     }
+
+    function increementUint8() public {
+        myUint8++;
+    }
+
+    function increementInt() public {
+        myInt++;
+    }
+
 }

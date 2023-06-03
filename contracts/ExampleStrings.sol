96f0f70209058da921b79e262b30cffad16e0681
Denis-Bez <v417459@yandex.ru>
2023-06-04 00:43:40 +0500

    Education Lesson 24

---
+++ contracts/ExampleStrings.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+contract ExampleStrings {
+
+    string public myString = "Hello, Hell";
+    bytes public myBytes = "Hello, Hell";
+
+    function setMyString(string memory _myString) public  {
+        myString = _myString;
+    }
+
+    function compareTwoString(string memory _myString) public view returns(bool) {
+        return keccak256(abi.encodePacked(myString)) == keccak256(abi.encodePacked(_myString));
+    }
+
+}

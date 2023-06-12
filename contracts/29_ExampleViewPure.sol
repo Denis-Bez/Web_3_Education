74d794d34f31495aeefffae8a414a7389f121ad0
Denis-Bez <v417459@yandex.ru>
2023-06-13 04:24:56 +0500

    Education Lesson 30

--- contracts/29_ExampleViewPure.sol
+++ contracts/29_ExampleViewPure.sol
@@ -6,14 +6,17 @@ contract ExampleViewPure {
 
     uint public myStorageVariable;
 
+    // View function
     function getMyStorageVariable() public view returns(uint) {
         return myStorageVariable;
     }
 
+    // Pure function
     function getAddition(uint a, uint b) public pure returns(uint) {
         return a+b;
     }
 
+    // Write function
     function setMyStorageVariable(uint _newVar) public returns(uint) {
         myStorageVariable = _newVar;
         return _newVar; // Bad idea

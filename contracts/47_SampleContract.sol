daa851a80369b489aa53ba42d331ed2ad80077e1
Denis-Bez <v417459@yandex.ru>
2023-07-03 11:57:48 +0500

    Education Lesson 47 (Chapter 2)

--- contracts/47_SampleContract.sol
+++ contracts/47_SampleContract.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.18;
 
 contract SampleContract {
 
-    string public MyString = "Hello, Hell!";
+    string public MyString = "Hello, Hell";
 
     function updateString(string memory _newString) public {
         MyString = _newString;

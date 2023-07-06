83dc22aed65585d0fe63b51d1f57e8e56aadde52
Denis-Bez <v417459@yandex.ru>
2023-07-06 11:28:42 +0500

    Education Lesson 51 (Chapter 2)

--- contracts/51_SampleFallback.sol
+++ contracts/51_SampleFallback.sol
@@ -4,4 +4,23 @@ pragma solidity 0.8.18;
 
 contract SampleFallback {
     
+    uint public lastValueSent;
+    string public lastFunctionCalled;
+
+    uint public myUint;
+
+    function setMyUint(uint _myNewUint) public {
+        myUint = _myNewUint;
+    }
+
+    // This function have only 2300 Gas. Can't receive data, only value
+    receive() external payable {
+        lastValueSent = msg.value;
+        lastFunctionCalled = "receive";
+    }
+
+    fallback() external payable {
+        lastValueSent = msg.value;
+        lastFunctionCalled = "fallback";
+    }
 }
